b02cabba41159815967752bd358faef0aead0e69
Ryan Nguyen <ryan.nguyen0720@gmail.com>
2023-11-14 23:07:54 +1300

    added title

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -5,7 +5,10 @@ const Home = () => {
   return (
     <section>
       <div className="overflow-hidden px-4 sm:px-6 mt-10 sm:flex sm:flex-col gap-4 md:justify-center md:items-center">
-        <TitleSection pill="✨ Your Workspace, Perfected" title="Platform" />
+        <TitleSection
+          pill="✨ Your Workspace, Perfected"
+          title="All-In-One Collaboration and Productivity Platform"
+        />
       </div>
     </section>
   )
