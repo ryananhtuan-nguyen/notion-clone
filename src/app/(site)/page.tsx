47c4cade673abe23c22744e67bf66364f4ff8a0d
Ryan Nguyen <ryan.nguyen0720@gmail.com>
2023-11-15 11:28:00 +1300

    pricing plans section

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -1,12 +1,24 @@
+import React from 'react'
+import Image from 'next/image'
+import { randomUUID } from 'crypto'
+
+import CustomCard from '@/components/CustomCard'
 import TitleSection from '@/components/landing-page/TitleSection'
 import Banner from '../../../public/appBanner.png'
 import Cal from '../../../public/cal.png'
+import Diamond from '../../../public/icons/diamond.svg'
+import CheckIcon from '../../../public/icons/check.svg'
 
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardTitle,
+} from '@/components/ui/card'
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
 import { Button } from '@/components/ui/button'
-import { CLIENTS } from '@/lib/constants'
-
-import Image from 'next/image'
-import React from 'react'
+import { CLIENTS, PRICING_CARDS, PRICING_PLANS, USERS } from '@/lib/constants'
+import { cn } from '@/lib/utils'
 
 const Home = () => {
   return (
@@ -18,7 +30,7 @@ const Home = () => {
         />
         <div className="bg-white p-[2px] mt-6 rounded-xl bg-gradient-to-r from-primary to-brand-primaryBlue sm:w-[300px]">
           <Button
-            variant="secondary"
+            variant="btn-secondary"
             className="w-full rounded-[10px] p-6 text-2xl bg-background"
           >
             Get Cypress Free
@@ -96,6 +108,118 @@ const Home = () => {
             personal and professional productivity needs."
             pill="Testimonials"
           />
+          {[...Array(2)].map((arr, index) => (
+            <div
+              key={randomUUID()}
+              className={cn(
+                'mt-10 flex flex-nowrap gap-6 self-start hover:paused',
+                {
+                  'flex-row-reverse': index === 1,
+                  'animate-[slide_250s_linear_infinite]': true,
+                  'animate-[slide_250s_linear_infinite_reverse]': index === 1,
+                  'ml-[100vw]': index === 1,
+                }
+              )}
+            >
+              {USERS.map((testimonial, index) => (
+                <CustomCard
+                  key={testimonial.name}
+                  className="w-[500px] shrink-0 rounded-xl dark:bg-gradient-to-t dark:from-border dark:to-background"
+                  cardHeader={
+                    <div className="flex items-center gap-4">
+                      <Avatar>
+                        <AvatarImage src={`/avatars/${index + 1}.png`} />
+                        <AvatarFallback>AV</AvatarFallback>
+                      </Avatar>
+                      <div>
+                        <CardTitle className="text-foreground">
+                          {testimonial.name}
+                        </CardTitle>
+                        <CardDescription className="dark:text-washed-purple-800">
+                          {testimonial.name.toLocaleLowerCase()}
+                        </CardDescription>
+                      </div>
+                    </div>
+                  }
+                  cardContent={
+                    <p className="dark:text-washed-purple-800">
+                      {testimonial.message}
+                    </p>
+                  }
+                ></CustomCard>
+              ))}
+            </div>
+          ))}
+        </div>
+      </section>
+      <section className="mt-20 sm:px-6 px-4">
+        <TitleSection
+          title="The Perfect Plan For You"
+          subheading="Experience all the benefits of our platform. Select a plan that suits your needs and take your productivity to new heights."
+          pill="Pricing"
+        />
+        <div className="flex flex-col-reverse sm:flex-row gap-4 justify-center sm:items-stretch items-center mt-10">
+          {PRICING_CARDS.map((card) => (
+            <CustomCard
+              key={card.planType}
+              className={cn(
+                'w-[300px] rounded-2xl dark:bg-black/95 background-blur-3xl relative',
+                {
+                  'border-brand-primaryPurple/70':
+                    card.planType === PRICING_PLANS.proplan,
+                }
+              )}
+              cardHeader={
+                <CardTitle className="text-2xl font-semibold">
+                  {card.planType === PRICING_PLANS.proplan && (
+                    <>
+                      <div className="hidden dark:block w-full blur-[120px] rounded-full h-32 absolute bg-brand-primaryPurple/80 -z-10 top-0" />
+                      <Image
+                        src={Diamond}
+                        alt="Proplan icon"
+                        className="absolute top-6 right-6"
+                      />
+                    </>
+                  )}
+                  {card.planType}
+                </CardTitle>
+              }
+              cardContent={
+                <CardContent className="p-0">
+                  <span className="font-normal text-2xl">${card.price}</span>
+                  {+card.price > 0 ? (
+                    <span className="dark:text-washed-purple-800 ml-1">
+                      /mo
+                    </span>
+                  ) : (
+                    ''
+                  )}
+                  <p className="dark:text-washed-purple-800">
+                    {card.description}
+                  </p>
+                  <Button
+                    className="whitespace-nowrap w-full mt-4"
+                    variant="btn-primary"
+                  >
+                    {card.planType === PRICING_PLANS.proplan
+                      ? 'Go Pro'
+                      : 'Get Started'}
+                  </Button>
+                </CardContent>
+              }
+              cardFooter={
+                <ul className="font-normal flex mb-2 flex-col gap-4">
+                  <small>{card.highlightFeature}</small>
+                  {card.features.map((feature) => (
+                    <li key={feature} className="flex items-center gap-2">
+                      <Image src={CheckIcon} alt="CheckIcon" />
+                      {feature}
+                    </li>
+                  ))}
+                </ul>
+              }
+            ></CustomCard>
+          ))}
         </div>
       </section>
     </>
