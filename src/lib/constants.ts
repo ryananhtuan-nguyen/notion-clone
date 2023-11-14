2025ed18be09dbf29145b6f77a9174e6cc196b9d
Ryan Nguyen <ryan.nguyen0720@gmail.com>
2023-11-15 11:22:49 +1300

    typo

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -116,7 +116,7 @@ export const PRICING_CARDS = [
     price: '0',
     description: 'Limited block trials  for teams',
     highlightFeature: '',
-    freatures: [
+    features: [
       'Unlimited blocks for teams',
       'Unlimited file uploads',
       '30 day page history',
@@ -128,7 +128,7 @@ export const PRICING_CARDS = [
     price: '12.99',
     description: 'Billed annually. $17 billed monthly',
     highlightFeature: 'Everything in free +',
-    freatures: [
+    features: [
       'Unlimited blocks for teams',
       'Unlimited file uploads',
       '1 year day page history',
