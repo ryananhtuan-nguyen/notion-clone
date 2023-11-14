1dafe5ddd8f56790c75b2b8d5b52dd1756dfcb6a
Ryan Nguyen <ryan.nguyen0720@gmail.com>
2023-11-14 22:09:53 +1300

    testing db migration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import db from '@/lib/supabase/db'
+db
 
 const inter = Inter({ subsets: ['latin'] })
 
