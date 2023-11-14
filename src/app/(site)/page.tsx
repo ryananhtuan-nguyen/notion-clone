23d7096f5162424681a6d3083a307e2bb71d9712
Ryan Nguyen <ryan.nguyen0720@gmail.com>
2023-11-14 23:01:14 +1300

    put title in home page

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -1,7 +1,14 @@
+import TitleSection from '@/components/landing-page/TitleSection'
 import React from 'react'
 
 const Home = () => {
-  return <div>Home</div>
+  return (
+    <section>
+      <div className="overflow-hidden px-4 sm:px-6 mt-10 sm:flex sm:flex-col gap-4 md:justify-center md:items-center">
+        <TitleSection pill="✨ Your Workspace, Perfected" title="Platform" />
+      </div>
+    </section>
+  )
 }
 
 export default Home
