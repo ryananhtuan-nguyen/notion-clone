cc9e8311b433d1f8b8bff931e0a1a51ef0e6d598
Ryan Nguyen <ryan.nguyen0720@gmail.com>
2023-11-15 11:56:56 +1300

    starting form

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -2,10 +2,26 @@
 
 import { useRouter } from 'next/navigation'
 import { useState } from 'react'
+import { SubmitHandler, useForm } from 'react-hook-form'
+import * as z from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { FormSchema } from '@/lib/types'
 
 const LoginPage = () => {
   const router = useRouter()
   const [submitError, setSubmitError] = useState('')
+
+  const form = useForm<z.infer<typeof FormSchema>>({
+    mode: 'onChange',
+    resolver: zodResolver(FormSchema),
+    defaultValues: { email: '', password: '' },
+  })
+
+  const isLoading = form.formState.isSubmitting
+
+  const onSubmit: SubmitHandler<z.infer<typeof FormSchema>> = async (
+    formData
+  ) => {}
 
   return <div>LoginPage</div>
 }
