81a1b20165a4176e7e7ed700b85b7c0088124345
Ryan Nguyen <ryan.nguyen0720@gmail.com>
2023-11-15 21:57:18 +1300

    auth actions

---
+++ src/lib/server-action/auth-actions.ts
@@ -0,0 +1,19 @@
+'use server'
+
+import { z } from 'zod'
+import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs'
+import { cookies } from 'next/headers'
+
+import { FormSchema } from '../types'
+
+export async function actionLoginUser({
+  email,
+  password,
+}: z.infer<typeof FormSchema>) {
+  const supabase = createRouteHandlerClient({ cookies })
+  const response = await supabase.auth.signInWithPassword({
+    email,
+    password,
+  })
+  return response
+}
