841237b1cad9b37e4373928330b643b129741b1a
Ryan Nguyen <ryan.nguyen0720@gmail.com>
2023-11-16 10:59:08 +1300

    grab props from server component

--- src/components/dashboard-setup/DashboardSetup.tsx
+++ src/components/dashboard-setup/DashboardSetup.tsx
@@ -1,13 +1,39 @@
+'use client'
 import { AuthUser } from '@supabase/supabase-js'
 import React from 'react'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '../ui/card'
 
 interface DashboardSetupProps {
   user: AuthUser
   subscription: {} | null
 }
 
-const DashboardSetup = () => {
-  return <div>DashboardSetup</div>
+const DashboardSetup: React.FC<DashboardSetupProps> = ({
+  user,
+  subscription,
+}) => {
+  return (
+    <Card className="w-[800px] h-screen sm:h-auto">
+      <CardHeader>
+        <CardTitle>Create a Workspace</CardTitle>
+        <CardDescription>
+          Let&apos;s create a private workspace to get you started. You can add
+          collaborators later from the workspace settings tab.
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <form onSubmit={() => {}}>
+          <div className="flex flex-col gap-4"></div>
+        </form>
+      </CardContent>
+    </Card>
+  )
 }
 
 export default DashboardSetup
