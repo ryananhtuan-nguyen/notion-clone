4f8f8c47e05aaed533131a324b70a315d1904616
Ryan Nguyen <ryan.nguyen0720@gmail.com>
2023-11-15 21:54:20 +1300

    customize button

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -12,7 +12,7 @@ const buttonVariants = cva(
     variants: {
       variant: {
         default:
-          ' whitespace-nowrap bg-primary text-lg text-primary-foreground shadow-2xl shadow-indigo-600/50 rounded-lg ',
+          'whitespace-nowrap bg-primary text-lg text-primary-foreground shadow-2xl shadow-indigo-600/50 rounded-lg ',
         destructive:
           'bg-destructive text-destructive-foreground hover:bg-destructive/90',
         outline:
