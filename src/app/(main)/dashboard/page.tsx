3c5a8cef3ba90aff480a0ae7152c04137f26cc4a
Ryan Nguyen <ryan.nguyen0720@gmail.com>
2023-11-16 10:58:53 +1300

    passed subscription down

--- src/app/(main)/dashboard/page.tsx
+++ src/app/(main)/dashboard/page.tsx
@@ -1,7 +1,35 @@
 import React from 'react'
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
+import { cookies } from 'next/headers'
+import db from '@/lib/supabase/db'
+import { redirect } from 'next/navigation'
+import DashboardSetup from '@/components/dashboard-setup/DashboardSetup'
+import { getUserSubscriptionStatus } from '@/lib/supabase/queries'
 
-const Page = () => {
-  return <div>Page</div>
+const DashboardPage = async () => {
+  const supabase = createServerComponentClient({ cookies })
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser()
+
+  if (!user) return
+
+  const workspace = await db.query.workspaces.findFirst({
+    where: (workspace, { eq }) => eq(workspace.workspaceOwner, user.id),
+  })
+
+  const { data: subscription, error: subscriptionError } =
+    await getUserSubscriptionStatus(user.id)
+
+  if (subscriptionError) return
+  if (!workspace)
+    return (
+      <div className="bg-backround h-screen w-screen flex justify-center items-center">
+        <DashboardSetup user={user} subscription={subscription} />
+      </div>
+    )
+  redirect(`/dashboard/${workspace.id}`)
 }
 
-export default Page
+export default DashboardPage
