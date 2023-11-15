5a800ba35f58401cd479b47b1f8140364cb271b7
Ryan Nguyen <ryan.nguyen0720@gmail.com>
2023-11-15 22:13:33 +1300

    shadcn separator

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,11 +1,26 @@
 'use client'
-
 import { useRouter } from 'next/navigation'
-import { useState } from 'react'
+import React, { useState } from 'react'
 import { SubmitHandler, useForm } from 'react-hook-form'
 import * as z from 'zod'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { FormSchema } from '@/lib/types'
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormMessage,
+} from '@/components/ui/form'
+import Link from 'next/link'
+import Image from 'next/image'
+import Logo from '../../../../public/cypresslogo.svg'
+import { Input } from '@/components/ui/input'
+import { Button } from '@/components/ui/button'
+import Loader from '@/components/Loader'
+import { Separator } from '@/components/ui/separator'
+import { actionLoginUser } from '@/lib/server-action/auth-actions'
 
 const LoginPage = () => {
   const router = useRouter()
@@ -21,9 +36,90 @@ const LoginPage = () => {
 
   const onSubmit: SubmitHandler<z.infer<typeof FormSchema>> = async (
     formData
-  ) => {}
+  ) => {
+    const { error } = await actionLoginUser(formData)
+    if (error) {
+      form.reset()
+      setSubmitError(error.message)
+    }
+    router.replace('/dashboard')
+  }
 
-  return <div>LoginPage</div>
+  return (
+    <Form {...form}>
+      <form
+        onChange={() => {
+          if (submitError) setSubmitError('')
+        }}
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="w-full sm:justify-center sm:w-[400px] space-y-6 flex flex-col"
+      >
+        <Link
+          href="/"
+          className="
+          w-full
+          flex
+          justify-left
+          items-center"
+        >
+          <Image src={Logo} alt="cypress Logo" width={50} height={50} />
+          <span
+            className="font-semibold
+          dark:text-white text-4xl first-letter:ml-2"
+          >
+            cypress.
+          </span>
+        </Link>
+        <FormDescription
+          className="
+        text-foreground/60"
+        >
+          An all-In-One Collaboration and Productivity Platform
+        </FormDescription>
+        <FormField
+          disabled={isLoading}
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormControl>
+                <Input type="email" placeholder="Email" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          disabled={isLoading}
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormControl>
+                <Input type="password" placeholder="Password" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        {submitError && <FormMessage>{submitError}</FormMessage>}
+        <Button
+          type="submit"
+          className="w-full p-6"
+          size="lg"
+          disabled={isLoading}
+        >
+          {!isLoading ? 'Login' : <Loader />}
+        </Button>
+        <span className="self-container">
+          Dont have an account?{' '}
+          <Link href="/signup" className="text-primary">
+            Sign Up
+          </Link>
+        </span>
+      </form>
+    </Form>
+  )
 }
 
 export default LoginPage
