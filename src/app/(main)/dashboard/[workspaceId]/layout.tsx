b4225350ea505bc5d6bb3a136b9db46301924ba7
Ryan Nguyen <ryan.nguyen0720@gmail.com>
2023-11-17 12:13:03 +1300

    layout for workspace id

---
+++ src/app/(main)/dashboard/[workspaceId]/layout.tsx
@@ -0,0 +1,22 @@
+import Sidebar from '@/components/sidebar/Sidebar'
+import { SubscriptionModalProvider } from '@/lib/providers/SubscriptionModalProvider'
+import { getActiveProductsWithPrice } from '@/lib/supabase/queries'
+import React from 'react'
+
+interface LayoutProps {
+  children: React.ReactNode
+  params: { workspaceId: string }
+}
+
+const Layout: React.FC<LayoutProps> = async ({ children, params }) => {
+  return (
+    <main className="flex overflow-hidden h-screen w-screen">
+      <Sidebar params={params} />
+      <div className="dark:border-Neutrals-12/70 border-l-[1px] w-full relative overflow-scroll">
+        {children}
+      </div>
+    </main>
+  )
+}
+
+export default Layout
