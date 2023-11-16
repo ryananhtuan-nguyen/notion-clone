5a75232119990c71061b1dfe9e68d04a9e6185cd
Ryan Nguyen <ryan.nguyen0720@gmail.com>
2023-11-17 09:05:44 +1300

    fixing bug

--- src/app/(main)/dashboard/layout.tsx
+++ src/app/(main)/dashboard/layout.tsx
@@ -1,3 +1,5 @@
+import { SubscriptionModalProvider } from '@/lib/providers/SubscriptionModalProvider'
+import { getActiveProductsWithPrice } from '@/lib/supabase/queries'
 import React from 'react'
 
 interface LayoutProps {
@@ -5,8 +7,18 @@ interface LayoutProps {
   params: any
 }
 
-const Layout: React.FC<LayoutProps> = ({ children, params }) => {
-  return <main className="flex overflow-hidden h-screen">{children}</main>
+const Layout: React.FC<LayoutProps> = async ({ children, params }) => {
+  const { data: products, error } = await getActiveProductsWithPrice()
+  // if (error) {
+  //   throw new Error()
+  // }
+  return (
+    <main className="flex over-hidden h-screen">
+      <SubscriptionModalProvider products={products}>
+        {children}
+      </SubscriptionModalProvider>
+    </main>
+  )
 }
 
 export default Layout
