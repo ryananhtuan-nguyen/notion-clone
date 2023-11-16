4b80a5fd1724d28c8fd94ce826d3382e2750591a
Ryan Nguyen <ryan.nguyen0720@gmail.com>
2023-11-17 00:07:58 +1300

    CreateWorkSpace form schema

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -6,3 +6,11 @@ export const FormSchema = z.object({
   }),
   password: z.string().describe('Password').min(1, 'Password is required'),
 })
+
+export const CreateWorkspaceFormSchema = z.object({
+  workspaceName: z
+    .string()
+    .describe('Workspace Name')
+    .min(1, 'Workspace name must be min of 1 character'),
+  logo: z.any(),
+})
