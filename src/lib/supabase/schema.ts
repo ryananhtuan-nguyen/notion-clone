d68e72c99f052ed594dc9bc50493256dbf0db9c8
Ryan Nguyen <ryan.nguyen0720@gmail.com>
2023-11-17 09:22:30 +1300

    updated schema

--- src/lib/supabase/schema.ts
+++ src/lib/supabase/schema.ts
@@ -1,3 +1,4 @@
+import { relations, sql } from 'drizzle-orm'
 import {
   boolean,
   integer,
@@ -7,15 +8,21 @@ import {
   timestamp,
   uuid,
 } from 'drizzle-orm/pg-core'
-import { prices, subscriptionStatus } from '../../../migrations/schema'
-import { sql } from 'drizzle-orm'
+import {
+  prices,
+  products,
+  subscriptionStatus,
+  users,
+} from '../../../migrations/schema'
 
 export const workspaces = pgTable('workspaces', {
   id: uuid('id').defaultRandom().primaryKey().notNull(),
   createdAt: timestamp('created_at', {
     withTimezone: true,
     mode: 'string',
-  }),
+  })
+    .defaultNow()
+    .notNull(),
   workspaceOwner: uuid('workspace_owner').notNull(),
   title: text('title').notNull(),
   iconId: text('icon_id').notNull(),
@@ -30,17 +37,19 @@ export const folders = pgTable('folders', {
   createdAt: timestamp('created_at', {
     withTimezone: true,
     mode: 'string',
-  }),
-  workspaceOwner: uuid('workspace_owner').notNull(),
+  })
+    .defaultNow()
+    .notNull(),
   title: text('title').notNull(),
   iconId: text('icon_id').notNull(),
   data: text('data'),
   inTrash: text('in_trash'),
-  logo: text('logo'),
   bannerUrl: text('banner_url'),
-  workspaceId: uuid('workspace_id').references(() => workspaces.id, {
-    onDelete: 'cascade',
-  }),
+  workspaceId: uuid('workspace_id')
+    .notNull()
+    .references(() => workspaces.id, {
+      onDelete: 'cascade',
+    }),
 })
 
 export const files = pgTable('files', {
@@ -48,23 +57,26 @@ export const files = pgTable('files', {
   createdAt: timestamp('created_at', {
     withTimezone: true,
     mode: 'string',
-  }),
-  workspaceOwner: uuid('workspace_owner').notNull(),
+  })
+    .defaultNow()
+    .notNull(),
   title: text('title').notNull(),
   iconId: text('icon_id').notNull(),
   data: text('data'),
   inTrash: text('in_trash'),
-  logo: text('logo'),
   bannerUrl: text('banner_url'),
-  workspaceId: uuid('workspace_id').references(() => workspaces.id, {
-    onDelete: 'cascade',
-  }),
-  folderId: uuid('folder_id').references(() => folders.id, {
-    onDelete: 'cascade',
-  }),
+  workspaceId: uuid('workspace_id')
+    .notNull()
+    .references(() => workspaces.id, {
+      onDelete: 'cascade',
+    }),
+  folderId: uuid('folder_id')
+    .notNull()
+    .references(() => folders.id, {
+      onDelete: 'cascade',
+    }),
 })
 
-//copy from drizzle schema. Keep development database up to date
 export const subscriptions = pgTable('subscriptions', {
   id: text('id').primaryKey().notNull(),
   userId: uuid('user_id').notNull(),
@@ -109,3 +121,31 @@ export const subscriptions = pgTable('subscriptions', {
     mode: 'string',
   }).default(sql`now()`),
 })
+
+export const collaborators = pgTable('collaborators', {
+  id: uuid('id').defaultRandom().primaryKey().notNull(),
+  workspaceId: uuid('workspace_id')
+    .notNull()
+    .references(() => workspaces.id, { onDelete: 'cascade' }),
+  createdAt: timestamp('created_at', {
+    withTimezone: true,
+    mode: 'string',
+  })
+    .defaultNow()
+    .notNull(),
+  userId: uuid('user_id')
+    .notNull()
+    .references(() => users.id, { onDelete: 'cascade' }),
+})
+
+//Dont Delete!!!
+export const productsRelations = relations(products, ({ many }) => ({
+  prices: many(prices),
+}))
+
+export const pricesRelations = relations(prices, ({ one }) => ({
+  product: one(products, {
+    fields: [prices.productId],
+    references: [products.id],
+  }),
+}))
