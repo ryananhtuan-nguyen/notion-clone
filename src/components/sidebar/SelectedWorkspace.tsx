6ad434ff325754597c47b87ab03231526ca0ff94
Ryan Nguyen <ryan.nguyen0720@gmail.com>
2023-11-17 13:31:15 +1300

    showing workspaces

---
+++ src/components/sidebar/SelectedWorkspace.tsx
@@ -0,0 +1,57 @@
+'use client'
+
+import React, { useEffect, useState } from 'react'
+import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
+
+import { workspace } from '@/lib/supabase/supabase.types'
+import Link from 'next/link'
+import Image from 'next/image'
+
+interface SelectedWorkspaceProps {
+  workspace: workspace
+  onClick?: (option: workspace) => void
+}
+
+const SelectedWorkspace: React.FC<SelectedWorkspaceProps> = ({
+  workspace,
+  onClick,
+}) => {
+  const supabase = createClientComponentClient()
+  const [workspaceLogo, setWorkspaceLogo] = useState('/cypresslogo.svg')
+
+  useEffect(() => {
+    if (workspace.logo) {
+      const path = supabase.storage
+        .from('workspace-logos')
+        .getPublicUrl(workspace.logo)?.data.publicUrl
+      setWorkspaceLogo(path)
+    }
+  }, [workspace])
+
+  return (
+    <Link
+      href={`/dashboard/${workspace.id}`}
+      onClick={() => {
+        if (onClick) {
+          onClick(workspace)
+        }
+      }}
+      className="flex rounded-md hover:bg-muted transition-all flex-row p-2 gap-4 justify-center cursor-pointer items-center my2"
+    >
+      <Image
+        src={workspaceLogo}
+        alt="workspace logo"
+        width={26}
+        height={26}
+        objectFit="cover"
+      />
+      <div className="flex flex-col">
+        <p className="text-lg overflow-hidden overflow-ellipsis whitespace-nowrap">
+          {workspace.title}
+        </p>
+      </div>
+    </Link>
+  )
+}
+
+export default SelectedWorkspace
