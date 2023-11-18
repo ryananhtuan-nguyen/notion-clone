7df591ac4c4cc332dba636961c7d06a3e4beee0d
Ryan Nguyen <ryan.nguyen0720@gmail.com>
2023-11-18 15:35:49 +1300

    done wrapper

--- src/components/global/TooltipComponent.tsx
+++ src/components/global/TooltipComponent.tsx
@@ -1,7 +1,29 @@
 import React from 'react'
 
-const TooltipComponent = () => {
-  return <div>TooltipComponent</div>
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from '../ui/tooltip'
+
+interface TooltipComponentProps {
+  children: React.ReactNode
+  message: string
+}
+
+const TooltipComponent: React.FC<TooltipComponentProps> = ({
+  children,
+  message,
+}) => {
+  return (
+    <TooltipProvider>
+      <Tooltip>
+        <TooltipTrigger>{children}</TooltipTrigger>
+        <TooltipContent>{message}</TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
+  )
 }
 
 export default TooltipComponent
