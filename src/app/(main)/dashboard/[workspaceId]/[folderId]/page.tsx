88fff86a1668ac4bc826ad6c392bf1d353fc45d0
Ryan Nguyen <ryan.nguyen0720@gmail.com>
2023-11-18 23:32:42 +1300

    rendering for folder

--- src/app/(main)/dashboard/[workspaceId]/[folderId]/page.tsx
+++ src/app/(main)/dashboard/[workspaceId]/[folderId]/page.tsx
@@ -1,7 +1,23 @@
-import React from 'react'
+export const dynamic = 'force-dynamic'
 
-const FolderPage = () => {
-  return <div>FolderPage</div>
+import React from 'react'
+import { redirect } from 'next/navigation'
+
+import { getFolderDetails } from '@/lib/supabase/queries'
+import QuillEditor from '@/components/quill-editor/QuillEditor'
+
+const FolderPage = async ({ params }: { params: { folderId: string } }) => {
+  const { data, error } = await getFolderDetails(params.folderId)
+  if (error || !data.length) redirect('/dashboard')
+  return (
+    <div className="relative">
+      <QuillEditor
+        dirType="folder"
+        fileId={params.folderId}
+        dirDetails={data[0] || {}}
+      />
+    </div>
+  )
 }
 
 export default FolderPage
