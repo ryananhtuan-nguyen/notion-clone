30df8efec1518f116bdf224997d9d1c15b309254
Ryan Nguyen <ryan.nguyen0720@gmail.com>
2023-11-18 22:21:05 +1300

    force dynamic export

--- src/app/(main)/dashboard/[workspaceId]/page.tsx
+++ src/app/(main)/dashboard/[workspaceId]/page.tsx
@@ -1,7 +1,28 @@
-import React from 'react'
+export const dynamic = 'force-dynamic'
 
-const WorkspacePage = () => {
-  return <div>WorkspacePage</div>
+import React from 'react'
+import { redirect } from 'next/navigation'
+
+import { getWorkspaceDetails } from '@/lib/supabase/queries'
+import QuillEditor from '@/components/quill-editor/QuillEditor'
+
+const WorkspacePage = async ({
+  params,
+}: {
+  params: { workspaceId: string }
+}) => {
+  const { data, error } = await getWorkspaceDetails(params.workspaceId)
+  if (error || !data.length) redirect('/dashboard')
+
+  return (
+    <div className="relative">
+      <QuillEditor
+        dirType="workspace"
+        fileId={params.workspaceId}
+        dirDetails={data[0] || {}}
+      />
+    </div>
+  )
 }
 
 export default WorkspacePage
