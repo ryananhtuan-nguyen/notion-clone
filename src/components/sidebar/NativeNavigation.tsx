b825e45f8d3ad5e41570f237ff1633954d7275e0
Ryan Nguyen <ryan.nguyen0720@gmail.com>
2023-11-18 20:26:52 +1300

    changed Link to setting modal

--- src/components/sidebar/NativeNavigation.tsx
+++ src/components/sidebar/NativeNavigation.tsx
@@ -4,6 +4,7 @@ import React from 'react'
 import CypressHomeIcon from '../icons/cypressHomeIcon'
 import CypressSettingsIcon from '../icons/cypressSettingsIcon'
 import CypressTrashIcon from '../icons/cypressTrashIcon'
+import Settings from '../settings/Settings'
 
 interface NativeNavigationProps {
   myWorkspaceId: string
@@ -26,15 +27,12 @@ const NativeNavigation: React.FC<NativeNavigationProps> = ({
             <span>My Workspace</span>
           </Link>
         </li>
-        <li>
-          <Link
-            className="group/native flex text-Neutrals/neutrals-7 transition-all gap-2"
-            href={`/dashboard/${myWorkspaceId}`}
-          >
+        <Settings>
+          <li className="group/native flex text-Neutrals/neutrals-7 transition-all gap-2 cursor-pointer">
             <CypressSettingsIcon />
             <span>Settings</span>
-          </Link>
-        </li>
+          </li>
+        </Settings>
         <li>
           <Link
             className="group/native flex text-Neutrals/neutrals-7 transition-all gap-2"
