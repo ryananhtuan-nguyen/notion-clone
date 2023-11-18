6880eef1bba7a33eda4669ed03a48116bb82d585
Ryan Nguyen <ryan.nguyen0720@gmail.com>
2023-11-18 15:32:49 +1300

    state for folders

--- src/components/sidebar/FoldersDropdownList.tsx
+++ src/components/sidebar/FoldersDropdownList.tsx
@@ -51,7 +51,7 @@ const FoldersDropdownList: React.FC<FoldersDropdownListProps> = ({
 
   return (
     <div className="flex sticky z-20 top-0 bg-background w-full h-10 group/title justify-between items-center pr-4 text-Neutrals/neutrals-8">
-      <span className="text-Neutrals-8 font-bold text-xs"></span>
+      <span className="text-Neutrals-8 font-bold text-xs">FOLDERS</span>
     </div>
   )
 }
