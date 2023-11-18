0aa8855057cf2676fbe7723966ca76d0d2bbc056
Ryan Nguyen <ryan.nguyen0720@gmail.com>
2023-11-18 20:49:55 +1300

    fetch data realtime

--- src/components/sidebar/WorkspaceDropdown.tsx
+++ src/components/sidebar/WorkspaceDropdown.tsx
@@ -43,6 +43,14 @@ const WorkspaceDropdown: React.FC<WorkspaceDropdownProps> = ({
     }
   }, [privateWorkspaces, sharedWorkspaces, collaboratingWorkspaces])
 
+  useEffect(() => {
+    const findSelectedWorkspace = state.workspaces.find(
+      (workspace) => workspace.id === defaultValue?.id
+    )
+
+    if (findSelectedWorkspace) setSelectedOption(findSelectedWorkspace)
+  }, [state, defaultValue])
+
   const handleSelect = (option: workspace) => {
     setSelectedOption(option)
     setIsOpen(false)
