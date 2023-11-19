acd154c3566b665ad77325bdfcec2032e4a38c4d
Ryan Nguyen <ryan.nguyen0720@gmail.com>
2023-11-19 14:46:41 +1300

    uploadbanner schema

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -14,3 +14,7 @@ export const CreateWorkspaceFormSchema = z.object({
     .min(1, 'Workspace name must be min of 1 character'),
   logo: z.any(),
 })
+
+export const UploadBannerFormSchema = z.object({
+  banner: z.string().describe('Banner Image'),
+})
