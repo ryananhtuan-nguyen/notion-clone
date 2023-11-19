8f070c1bd5b1438a0a286967dea65e0f91c01605
Ryan Nguyen <ryan.nguyen0720@gmail.com>
2023-11-20 10:54:25 +1300

    handling error

--- src/app/(main)/dashboard/layout.tsx
+++ src/app/(main)/dashboard/layout.tsx
@@ -1,5 +1,6 @@
 import { SubscriptionModalProvider } from '@/lib/providers/SubscriptionModalProvider'
 import { getActiveProductsWithPrice } from '@/lib/supabase/queries'
+import { redirect } from 'next/navigation'
 import React from 'react'
 interface LayoutProps {
   children: React.ReactNode
@@ -8,9 +9,10 @@ interface LayoutProps {
 
 const Layout: React.FC<LayoutProps> = async ({ children, params }) => {
   const { data: products, error } = await getActiveProductsWithPrice()
-  // if (error) {
-  //   throw new Error()
-  // }
+  if (error) {
+    redirect('/dashboard')
+  }
+  console.log('DATA', products, 'ERROR', error)
 
   return (
     <main className="flex overflow-hidden h-screen">
