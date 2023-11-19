0ef8ee8932628f8e43f7e383b38fa5ad385cc7c5
Ryan Nguyen <ryan.nguyen0720@gmail.com>
2023-11-19 14:46:16 +1300

    added form

---
+++ src/components/banner-upload/BannerUpload.tsx
@@ -0,0 +1,37 @@
+import React from 'react'
+
+import { File, Folder, workspace } from '@/lib/supabase/supabase.types'
+import {
+  appFoldersType,
+  appWorkspacesType,
+} from '@/lib/providers/state-provider'
+import CustomDialogTrigger from '../global/CustomDialogTrigger'
+import BannerUploadForm from './BannerUploadForm'
+
+interface BannerUploadProps {
+  children: React.ReactNode
+  className?: string
+  dirType: 'workspace' | 'folder' | 'file'
+  id: string
+  details: appWorkspacesType | appFoldersType | File | workspace | Folder
+}
+
+const BannerUpload: React.FC<BannerUploadProps> = ({
+  children,
+  className,
+  details,
+  dirType,
+  id,
+}) => {
+  return (
+    <CustomDialogTrigger
+      header="Upload Banner"
+      content={<BannerUploadForm />}
+      className={className}
+    >
+      {children}
+    </CustomDialogTrigger>
+  )
+}
+
+export default BannerUpload
