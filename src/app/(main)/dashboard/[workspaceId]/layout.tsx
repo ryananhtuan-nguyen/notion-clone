07a2f9580588d6296ecc528f23e3a1eb3a9969cc
Ryan Nguyen <ryan.nguyen0720@gmail.com>
2023-11-19 20:04:01 +1300

    addeed mobile sidebar

--- src/app/(main)/dashboard/[workspaceId]/layout.tsx
+++ src/app/(main)/dashboard/[workspaceId]/layout.tsx
@@ -1,3 +1,4 @@
+import MobileSidebar from '@/components/sidebar/MobileSidebar'
 import Sidebar from '@/components/sidebar/Sidebar'
 import { SubscriptionModalProvider } from '@/lib/providers/SubscriptionModalProvider'
 import { getActiveProductsWithPrice } from '@/lib/supabase/queries'
@@ -12,6 +13,9 @@ const Layout: React.FC<LayoutProps> = async ({ children, params }) => {
   return (
     <main className="flex overflow-hidden h-screen w-screen">
       <Sidebar params={params} />
+      <MobileSidebar>
+        <Sidebar params={params} className="w-screen inline-block sm:hidden" />
+      </MobileSidebar>
       <div className="dark:border-Neutrals-12/70 border-l-[1px] w-full relative overflow-scroll">
         {children}
       </div>
