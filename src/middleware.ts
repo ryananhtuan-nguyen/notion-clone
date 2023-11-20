5618c095e53aa0d12df27f3f83db5eaa284d29f3
Ryan Nguyen <ryan.nguyen0720@gmail.com>
2023-11-20 13:16:06 +1300

    fixing middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,13 +3,10 @@ import { NextRequest, NextResponse } from 'next/server'
 
 export async function middleware(req: NextRequest) {
   const res = NextResponse.next()
-
   const supabase = createMiddlewareClient({ req, res })
-
   const {
     data: { session },
   } = await supabase.auth.getSession()
-
   if (req.nextUrl.pathname.startsWith('/dashboard')) {
     if (!session) {
       return NextResponse.redirect(new URL('/login', req.url))
@@ -36,6 +33,5 @@ export async function middleware(req: NextRequest) {
       return NextResponse.redirect(new URL('/dashboard', req.url))
     }
   }
-
   return res
 }
