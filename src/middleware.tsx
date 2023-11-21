9584acf75398edd38ac1afdde48745e19d152ac9
Ryan Nguyen <ryan.nguyen0720@gmail.com>
2023-11-21 22:03:03 +1300

    debugging

--- src/middleware.tsx
+++ src/middleware.tsx
@@ -43,5 +43,5 @@ export async function middleware(req: NextRequest) {
   }
 
   console.log('LINE45 middleware, res.header', res.headers.entries())
-  return NextResponse.redirect(`${process.env.NEXT_PUBLIC_SITE_URL}/dashboard`)
+  return res
 }
