59bae281b621543e6822b4162af8f88cf645fb58
ryananhtuan-nguyen <ryan.nguyen0720@gmail.com>
2023-12-11 08:27:19 +1300

    style: remove absolute

--- src/components/global/ModeToggle.tsx
+++ src/components/global/ModeToggle.tsx
@@ -1,28 +1,38 @@
 'use client'
 import { useTheme } from 'next-themes'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Button } from '../ui/button'
 import { Moon, Sun } from 'lucide-react'
 
 const ModeToggle = () => {
   const { setTheme, theme } = useTheme()
+  const [isMounted, setIsMounted] = useState(false)
+  useEffect(() => {
+    setIsMounted(true)
+  }, [])
+
+  if (!isMounted) {
+    return null
+  }
   return (
     <Button
       variant={'outline'}
       size="icon"
       onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
     >
-      <Sun
-        className="h-[1.2rem] 
+      {theme === 'light' ? (
+        <Sun
+          className="h-[1.2rem] 
       w-[1.2rem]
        rotate-0 
        scale-100 
        transition-all 
        dark:-rotate-90 
        dark:scale-0"
-      />
-      <Moon
-        className="absolute
+        />
+      ) : (
+        <Moon
+          className="
        h-[1.2rem] 
        w-[1.2rem] 
        rotate-90 
@@ -30,7 +40,8 @@ const ModeToggle = () => {
        transition-all
        dark:rotate-0 
        dark:scale-100"
-      />
+        />
+      )}
     </Button>
   )
 }
