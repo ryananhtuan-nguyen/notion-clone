7020d33855cb72bc7546a30550652cd705febd6d
ryananhtuan-nguyen <ryan.nguyen0720@gmail.com>
2023-12-11 17:59:18 +1300

    chore: remove console log

--- src/middleware.tsx
+++ src/middleware.tsx
@@ -7,9 +7,6 @@ export async function middleware(req: NextRequest) {
   const {
     data: { session },
   } = await supabase.auth.getSession()
-  console.log(req.url, 'Req Url LINE 10 middleware')
-
-  console.log('LINE 12 MIDDLEWARE< SESSION', session)
 
   if (req.nextUrl.pathname.startsWith('/dashboard')) {
     if (!session) {
@@ -17,9 +14,6 @@ export async function middleware(req: NextRequest) {
     }
   }
 
-  console.log('[LINE 18 MIDDLEWARE, req.nextURL.pathname', req.nextUrl.pathname)
-
-  console.log('LINE 22 MIDDLEWARE, req.url', req.url)
   const emailLinkError = 'Email link is invalid or has expired'
   if (
     req.nextUrl.searchParams.get('error_description') === emailLinkError &&
@@ -42,6 +36,5 @@ export async function middleware(req: NextRequest) {
     }
   }
 
-  console.log('LINE45 middleware, res.header', res)
   return res
 }
