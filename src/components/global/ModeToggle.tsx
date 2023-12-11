b809a6a58a00cadec56e44dc9f359bf0cd307b29
ryananhtuan-nguyen <ryan.nguyen0720@gmail.com>
2023-12-11 21:27:47 +1300

    fix: debugging

--- src/components/global/ModeToggle.tsx
+++ src/components/global/ModeToggle.tsx
@@ -1,38 +1,28 @@
 'use client'
 import { useTheme } from 'next-themes'
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import { Button } from '../ui/button'
 import { Moon, Sun } from 'lucide-react'
 
 const ModeToggle = () => {
   const { setTheme, theme } = useTheme()
-  const [isMounted, setIsMounted] = useState(false)
-  useEffect(() => {
-    setIsMounted(true)
-  }, [])
-
-  if (!isMounted) {
-    return null
-  }
   return (
     <Button
       variant={'outline'}
       size="icon"
       onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
     >
-      {theme === 'light' ? (
-        <Sun
-          className="h-[1.2rem] 
+      <Sun
+        className="h-[1.2rem] 
       w-[1.2rem]
        rotate-0 
        scale-100 
        transition-all 
        dark:-rotate-90 
        dark:scale-0"
-        />
-      ) : (
-        <Moon
-          className="
+      />
+      <Moon
+        className="absolute
        h-[1.2rem] 
        w-[1.2rem] 
        rotate-90 
@@ -40,8 +30,7 @@ const ModeToggle = () => {
        transition-all
        dark:rotate-0 
        dark:scale-100"
-        />
-      )}
+      />
     </Button>
   )
 }
